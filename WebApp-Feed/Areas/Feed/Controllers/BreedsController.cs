270d407ef684627d6775e62726730052f62c876b
owlN-21 <nastakharina21@gmail.com>
2025-06-05 10:36:40 +0300

    add DELETE

--- WebApp-Feed/Areas/Feed/Controllers/BreedsController.cs
+++ WebApp-Feed/Areas/Feed/Controllers/BreedsController.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 
 namespace WebApp_Feed.Areas.Feed.Controllers
-    {
+{
     [Route("api/[controller]")]
     [ApiController]
     public class BreedsController : ControllerBase
@@ -21,25 +21,47 @@ namespace WebApp_Feed.Areas.Feed.Controllers
             _context = context;
         }
 
+        // GET: api/Breeds
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Breed>>> GetBreeds()
         {
-            return await _context.Breeds.Include(b => b.Images).ToListAsync();
+            return await _context.Breeds
+                .Include(b => b.Images)
+                .Include(b => b.Temperaments)
+                .ToListAsync();
         }
 
+        // GET: api/Breeds/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Breed>> GetBreed(int id)
         {
             var breed = await _context.Breeds
-                                    .Include(b => b.Images)
-                                    .Include(b => b.Temperaments)
-                                    .FirstOrDefaultAsync(b => b.Id == id);
+                .Include(b => b.Images)
+                .Include(b => b.Temperaments)
+                .FirstOrDefaultAsync(b => b.Id == id);
 
             if (breed == null)
                 return NotFound();
 
-
             return breed;
+        }
+
+        // DELETE: api/Breeds/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBreed(int id)
+        {
+            var breed = await _context.Breeds
+                .Include(b => b.Images)
+                .Include(b => b.Temperaments)
+                .FirstOrDefaultAsync(b => b.Id == id);
+
+            if (breed == null)
+                return NotFound();
+
+            _context.Breeds.Remove(breed);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 
